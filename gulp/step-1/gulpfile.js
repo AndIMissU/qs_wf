488df378a67afc11a1d550150827fa4bdd70cfb8
AndIMissU <784738601@qq.com>
2017-12-06 23:29:20 +0800

    更新一下gulp的操作

--- gulp/step-1/gulpfile.js
+++ gulp/step-1/gulpfile.js
@@ -1,6 +1,32 @@
 var gulp = require('gulp');
+// 集成 babel 编译
+var babel = require('gulp-babel');
+// 进行压缩
+var uglify = require('gulp-uglify');
+//引入cleancss
+var cleanCss = require('gulp-clean-css');
+//引入 concat
+var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  gulp.src('./src/main.js')
-    .pipe(gulp.dest('./dist'))
+gulp.task('jsmin', function() {
+  return gulp.src(['./src/js/main.js','./src/js/moon.js', './src/js/star.js'])
+    .pipe(babel({presets: ['env']}))
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
 });
+gulp.task('cssmin', function() {
+  return gulp.src(['./src/styles/btn.css', './src/styles/common.css'])
+  .pipe(concat('all.min.css'))
+  .pipe(cleanCss())
+  .pipe(gulp.dest('./dist/css'))
+});
+//监听文件改变
+gulp.task('watch',['jsmin', 'cssmin'], function(){
+  gulp.watch('./src/js/*.js',['jsmin']);
+  gulp.watch('./src/styles/*.css',['cssmin']);
+});
+//合并任务
+gulp.task('default',['watch']);
+
+//合并文件
